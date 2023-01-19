f3ac17df28c56d34c4f9ad237e5eda37c8fc2a57
Skullheadx <704277@pdsb.net>
2023-01-19 18:59:33 -0500

    Update main.py

--- main.py
+++ main.py
@@ -10,8 +10,8 @@ x = np.arange(-10, 10, 0.0005)
 # Equations
 y1 = 2 * np.sqrt((-abs(abs(x) - 1)) * abs(3 - abs(x)) / ((abs(x) - 1) * (3 - abs(x)))) * (
         1 + abs(abs(x) - 3) / (abs(x) - 3)) * np.sqrt(1 - (x / 7) ** 2) + (
-            5 + 0.97 * (abs(x - 0.5) + abs(x + 0.5)) - 3 * (abs(x - 0.75) + abs(x + 0.75))) * (
-            1 + abs(1 - abs(x)) / (1 - abs(x)))
+             5 + 0.97 * (abs(x - 0.5) + abs(x + 0.5)) - 3 * (abs(x - 0.75) + abs(x + 0.75))) * (
+             1 + abs(1 - abs(x)) / (1 - abs(x)))
 y2 = (-3) * np.sqrt(1 - (x / 7) ** 2) * np.sqrt(abs(abs(x) - 4) / (abs(x) - 4))
 y3 = abs(x / 2) - 0.0913722 * x ** 2 - 3 + np.sqrt(1 - (abs(abs(x) - 2) - 1) ** 2)
 y4 = (2.71052 + 1.5 - 0.5 * abs(x) - 1.35526 * np.sqrt(4 - (abs(x) - 1) ** 2)) * np.sqrt(abs(abs(x) - 1) /
@@ -27,7 +27,7 @@ plt.title('Batman')
 
 # Save to batman.png image output
 plt.savefig("batman.png", bbox_inches='tight')
-
+plt.ylim(ymax=5, ymin=-4)
 # show the graph
 plt.show()
 
